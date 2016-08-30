2f66d6f5a4a7b364729a4a236a03a2da2b6ff38f
xulipaxulepa <aofnas@hotmail.com>
2016-08-29 21:31:26 -0300

    mudando o form para textarea com o tinymce

--- application/views/componentesFixos/header.php
+++ application/views/componentesFixos/header.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="theme-color" content="#3b4ba7">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#3b4ba7">
 
-    <link REL="SHORTCUT ICON" HREF="<?php echo base_url('icon/icon.ico') ?>">
+        <link REL="SHORTCUT ICON" HREF="<?php echo base_url('icon/icon.ico') ?>">
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="<?php echo base_url('lib/bootstrap/css/bootstrap.min.css') ?>">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="<?php echo base_url('lib/bootstrap/css/bootstrap.min.css') ?>">
 
-    <!-- MyStyle -->
-    <link rel="stylesheet" href="<?php echo base_url('lib/style/style.css') ?>">
+        <!-- MyStyle -->
+        <link rel="stylesheet" href="<?php echo base_url('lib/style/style.css') ?>">
 
-    <title> SGAP</title>
-</head>
-<body>
-        
+        <!-- TinyMCE -->
+        <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+        <script>tinymce.init({selector: 'textarea'});</script>
 
-         
+        <title> SGAP</title>
+    </head>
+    <body>
+
+
+
--- application/views/periodico/cadastrar.php
+++ application/views/periodico/cadastrar.php
@@ -17,31 +17,31 @@ endif;
                     <?php
                     echo form_open('periodico/cadastrar');
                     echo form_label('Titulo (*)') . "<br />";
-                    echo form_input(array('id' => 'titulo', 'name' => 'titulo', 'class' => 'form-control', 'placeholder' => 'Titulo do artigo'), set_value('titulo')) . "<br />";
+                    echo form_textarea(array('id' => 'titulo', 'name' => 'titulo', 'class' => 'form-control', 'placeholder' => 'Titulo do artigo'), set_value('titulo')) . "<br />";
                     echo form_label('Autores (*)') . "<br />";
-                    echo form_input(array('id' => 'autores', 'name' => 'autores', 'class' => 'form-control', 'placeholder' => 'Autores do artigo'), set_value('autores')) . "<br />";
+                    echo form_textarea(array('id' => 'autores', 'name' => 'autores', 'class' => 'form-control', 'placeholder' => 'Autores do artigo'), set_value('autores')) . "<br />";
                     echo form_label('Resumo (*)') . "<br />";
-                    echo form_input(array('id' => 'resumo', 'name' => 'resumo', 'class' => 'form-control', 'placeholder' => 'Resumo do artigo'), set_value('resumo')) . "<br />";
+                    echo form_textarea(array('id' => 'resumo', 'name' => 'resumo', 'class' => 'form-control', 'placeholder' => 'Resumo do artigo'), set_value('resumo')) . "<br />";
                     echo form_label('Problema (*)') . "<br />";
-                    echo form_input(array('id' => 'problema', 'name' => 'problema', 'class' => 'form-control', 'placeholder' => 'Problematização do artigo'), set_value('problema')) . "<br />";
+                    echo form_textarea(array('id' => 'problema', 'name' => 'problema', 'class' => 'form-control', 'placeholder' => 'Problematização do artigo'), set_value('problema')) . "<br />";
                     echo form_label('Objetivo (*)') . "<br />";
-                    echo form_input(array('id' => 'objetivo', 'name' => 'objetivo', 'class' => 'form-control', 'placeholder' => 'Objetivo do artigo'), set_value('objetivo')) . "<br />";
+                    echo form_textarea(array('id' => 'objetivo', 'name' => 'objetivo', 'class' => 'form-control', 'placeholder' => 'Objetivo do artigo'), set_value('objetivo')) . "<br />";
                     echo form_label('Teórico (*)') . "<br />";
-                    echo form_input(array('id' => 'teorico', 'name' => 'teorico', 'class' => 'form-control', 'placeholder' => 'Teoria proposta no artigo'), set_value('teorico')) . "<br />";
+                    echo form_textarea(array('id' => 'teorico', 'name' => 'teorico', 'class' => 'form-control', 'placeholder' => 'Teoria proposta no artigo'), set_value('teorico')) . "<br />";
                     echo form_label('Metodologia (*)') . "<br />";
-                    echo form_input(array('id' => 'metodo', 'name' => 'metodo', 'class' => 'form-control', 'placeholder' => 'Metodologia do artigo'), set_value('metodo')) . "<br />";
+                    echo form_textarea(array('id' => 'metodo', 'name' => 'metodo', 'class' => 'form-control', 'placeholder' => 'Metodologia do artigo'), set_value('metodo')) . "<br />";
                     echo form_label('Desenvolvimento (*)') . "<br />";
-                    echo form_input(array('id' => 'desenvolvimento', 'name' => 'desenvolvimento', 'class' => 'form-control', 'placeholder' => 'Desenvolvimento do artigo'), set_value('desenvolvimento')) . "<br />";
+                    echo form_textarea(array('id' => 'desenvolvimento', 'name' => 'desenvolvimento', 'class' => 'form-control', 'placeholder' => 'Desenvolvimento do artigo'), set_value('desenvolvimento')) . "<br />";
                     echo form_label('Resultado (*)') . "<br />";
-                    echo form_input(array('id' => 'resultado', 'name' => 'resultado', 'class' => 'form-control', 'placeholder' => 'Resultados do artigo'), set_value('resultado')) . "<br />";
+                    echo form_textarea(array('id' => 'resultado', 'name' => 'resultado', 'class' => 'form-control', 'placeholder' => 'Resultados do artigo'), set_value('resultado')) . "<br />";
                     echo form_label('Analise (*)') . "<br />";
-                    echo form_input(array('id' => 'analise', 'name' => 'analise', 'class' => 'form-control', 'placeholder' => 'Analise do artigo'), set_value('analise')) . "<br />";
+                    echo form_textarea(array('id' => 'analise', 'name' => 'analise', 'class' => 'form-control', 'placeholder' => 'Analise do artigo'), set_value('analise')) . "<br />";
                     echo form_label('Conclusão (*)') . "<br />";
-                    echo form_input(array('id' => 'conclusao', 'name' => 'conclusao', 'class' => 'form-control', 'placeholder' => 'Conclusão do artigo'), set_value('conclusao')) . "<br />";
+                    echo form_textarea(array('id' => 'conclusao', 'name' => 'conclusao', 'class' => 'form-control', 'placeholder' => 'Conclusão do artigo'), set_value('conclusao')) . "<br />";
                     echo form_label('Pontos Fortes') . "<br />";
-                    echo form_input(array('id' => 'pforte', 'name' => 'pforte', 'class' => 'form-control', 'placeholder' => 'Pontos Fortes do artigo'), set_value('pforte')) . "<br />";
+                    echo form_textarea(array('id' => 'pforte', 'name' => 'pforte', 'class' => 'form-control', 'placeholder' => 'Pontos Fortes do artigo'), set_value('pforte')) . "<br />";
                     echo form_label('Pontos Fracos') . "<br />";
-                    echo form_input(array('id' => 'pfraco', 'name' => 'pfraco', 'class' => 'form-control', 'placeholder' => 'Pontos Fracos do artigo'), set_value('pfraco')) . "<br />";
+                    echo form_textarea(array('id' => 'pfraco', 'name' => 'pfraco', 'class' => 'form-control', 'placeholder' => 'Pontos Fracos do artigo'), set_value('pfraco')) . "<br />";
                     
                     ?>
                     <?php
