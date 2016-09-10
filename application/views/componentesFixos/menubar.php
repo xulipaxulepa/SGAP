a974b978db558dc47390fee6a261f3f42ceab41c
xulipaxulepa <aofnas@hotmail.com>
2016-09-09 22:52:52 -0300

    inserção da imagem localmente e tentativa de resolução para alinhar o form no menubar

--- application/views/componentesFixos/menubar.php
+++ application/views/componentesFixos/menubar.php
@@ -7,7 +7,7 @@ endif;
 <br>
 
 <nav class="navbar navbar-default">
-    <div class="navbar-header">
+    <div class="navbar-header" id="menubar">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
                 data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
